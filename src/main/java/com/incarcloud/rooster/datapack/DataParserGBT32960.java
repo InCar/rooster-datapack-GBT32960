da1881002413c1b88d1c22ad3288ff66c8c73d0d
youbeiwuhuan <fan794919764@126.com>
2017-06-30 15:50:17 +0800

    IDataPaser 升级

--- src/main/java/com/incarcloud/rooster/datapack/DataParserGBT32960.java
+++ src/main/java/com/incarcloud/rooster/datapack/DataParserGBT32960.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class DataParserGBT32960 implements IDataParser {
 
     @Override
-    public List<DataPack> extract(ByteBuf buffer){
+    public List<DataPack> extract(ByteBuf buffer) {
         /*  0,1: start with '##'(0x23 0x23)
               2: 命令
               3: 应答标志
@@ -31,36 +31,35 @@ public class DataParserGBT32960 implements IDataParser {
         // 4. 验证BCC. 如果验证失败,回到1
 
         // 包最小也得有25字节,没有25字节就不需要再处理了
-        while(buffer.readableBytes() >= frameLen){
+        while (buffer.readableBytes() >= frameLen) {
 
             // 如果找到'##',那么'##'之前的丢弃掉,如果没找到,整个丢弃掉
             boolean bFound = false;
-            for(int i=buffer.readerIndex();i<buffer.writerIndex()-1;i++){
-                if(buffer.getByte(i) == (byte)0x23 && buffer.getByte(i+1) == (byte)0x23){
+            for (int i = buffer.readerIndex(); i < buffer.writerIndex() - 1; i++) {
+                if (buffer.getByte(i) == (byte) 0x23 && buffer.getByte(i + 1) == (byte) 0x23) {
                     bFound = true;
                     buffer.skipBytes(i);
                     break;
                 }
             }
-            if(!bFound) buffer.skipBytes(buffer.readableBytes() - 1);
+            if (!bFound) buffer.skipBytes(buffer.readableBytes() - 1);
 
             // 获取BCC字节
-            if(bFound && buffer.readableBytes() >= frameLen){
+            if (bFound && buffer.readableBytes() >= frameLen) {
                 int dataLen = buffer.getUnsignedShort(22);
-                if(buffer.readableBytes() < dataLen + frameLen) break;
+                if (buffer.readableBytes() < dataLen + frameLen) break;
 
                 // 验证BCC异或校验码
                 byte bcc = 0x00;
-                for(int j=2;j<dataLen+frameLen-1;j++) bcc ^= buffer.getByte(j);
-                if(bcc == buffer.getByte(dataLen+frameLen-1)){
+                for (int j = 2; j < dataLen + frameLen - 1; j++) bcc ^= buffer.getByte(j);
+                if (bcc == buffer.getByte(dataLen + frameLen - 1)) {
                     // okay!
                     DataPack pack = new DataPack("china", "gbt32960", "1.0.0");
-                    ByteBuf buf = buffer.slice(0, dataLen+frameLen);
+                    ByteBuf buf = buffer.slice(0, dataLen + frameLen);
                     pack.setBuf(buf);
                     listPacks.add(pack);
-                    buffer.skipBytes(dataLen+frameLen);
-                }
-                else{
+                    buffer.skipBytes(dataLen + frameLen);
+                } else {
                     // 只能跳过1个字节,因为有可能出现这种3个连续的#,而从第2个#开始算起,恰好是一个正常的数据包的情况 ###.....
                     buffer.skipBytes(1);
                 }
@@ -78,5 +77,11 @@ public class DataParserGBT32960 implements IDataParser {
     @Override
     public void destroyResponse(ByteBuf responseBuf) {
 
+    }
+
+
+    @Override
+    public List<DataPackTarget> extractBody(DataPack dataPack) {
+        throw new UnsupportedOperationException();
     }
 }
